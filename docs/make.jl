17d5e8731f9eff03556afe92ca7b83a25f19bed8
Davide Ferracin <davide.ferracin@protonmail.com>
2024-11-26 12:31:04 +0200

    Files generated by PkgTemplates
    PkgTemplates version: 0.7.53

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using PauliStrings
+using Documenter
+
+DocMeta.setdocmeta!(PauliStrings, :DocTestSetup, :(using PauliStrings); recursive=true)
+
+makedocs(;
+    modules=[PauliStrings],
+    authors="Davide Ferracin <davide.ferracin@protonmail.com> and contributors",
+    sitename="PauliStrings.jl",
+    format=Documenter.HTML(;
+        edit_link="main",
+        assets=String[],
+    ),
+    pages=[
+        "Home" => "index.md",
+    ],
+)
---
+++ docs/src/index.md
@@ -0,0 +1,14 @@
+```@meta
+CurrentModule = PauliStrings
+```
+
+# PauliStrings
+
+Documentation for [PauliStrings](https://github.com/phaerrax/PauliStrings.jl).
+
+```@index
+```
+
+```@autodocs
+Modules = [PauliStrings]
+```
